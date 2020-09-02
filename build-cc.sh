17f75b1a03e57f0550f75ebbb713a0bfe78a33bf
mexuaz <mexuaz@outlook.com>
2020-09-01 23:13:43 -0700

    organized projects names

--- build-cc.sh
+++ build-cc.sh
@@ -2,8 +2,13 @@
 
 MAINDIR="AccTrussDecomposition"
 
-ALLAPPS=("opt-truss-decomp" "opt-offload-large-data" "opt-cpu-large-data" "opt-cpu-large-data" "msp")
-#ALLAPPS+=( "opt-truss-decomp-offload"  "h-idx")
+ALLAPPS=( "opt-truss-decomp" )
+ALLAPPS+=( "opt-truss-decomp-offload" ) # cuda problems with this project
+ALLAPPS+=( "opt-offload-large-data" )
+ALLAPPS+=( "opt-cpu-large-data" )
+ALLAPPS+=( "opt-cpu-large-data" )
+ALLAPPS+=( "msp" )
+ALLAPPS+=( "h-idx") # C++ problems with this projects
 
 BUILDDIR="build-acc"
 
@@ -12,33 +17,33 @@ CMAKEOPT="-DBUILD_SERIAL=ON -DPLAYGROUND=OFF -DLEGACY_PKT_ALL=ON"
 cd $HOME
 
 if [[ -d "$MAINDIR" ]]; then
-	echo ">>> Syncing repository ..."
+	echo ">>> Repository Syncing ..."
 	cd "$MAINDIR"
 	git reset --hard
 	git pull
 	cd ..
 else
-	echo ">>> Cloning repository ..."
+	echo ">>> Repository Cloning ..."
 	git clone git@github.com:mexuaz/$MAINDIR
 fi
-echo "<<<"
+echo "<<< Repository."
 
 if [[ -d "$BUILDDIR" ]]; then
     echo ">>> Removing old build directory."
     rm -rf "$BUILDDIR"
-    echo "<<<"
+    echo "<<< Old build directory."
 fi
 
 echo ">>> Creating new build directory."
 mkdir "$BUILDDIR"
 cd "$BUILDDIR"
-echo "<<<"
+echo "<<< New build directory."
 
 echo ">>> Reloading ComputeCanada essential modules ..."
 module --force purge
 module load nixpkgs gcc/8.3.0 cuda/10.1 cudnn/7.6.5 tbb/2018_U5 cmake/3.16.3 python/3.8.2 boost/1.68.0 openmpi/4.0.1
 module list
-echo "<<<"
+echo "<<< Essential Modules."
 
 echo ">>> Building all projects ..."
 
@@ -48,10 +53,10 @@ for PRG in ${ALLAPPS[*]}; do
   cd "$PRG"
   cmake "$HOME/$MAINDIR/$PRG" "$CMAKEOPT"
   make -j
-  echo "<<"
+  echo "<< Building $PRG"
 done
 
-echo "<<<"
+echo "<<< Build All projects."
 
 
 
